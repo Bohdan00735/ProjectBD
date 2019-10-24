4041b5d241683e365cd49a49ea4d637bbeed8af3
MAXSSYPE <dudkamv@gmail.com>
2019-10-24 23:11:03 +0300

    first_push

--- app/src/main/java/com/bd/projectbd/MainActivity.kt
+++ app/src/main/java/com/bd/projectbd/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.bd.projectbd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
+    lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timer.start()
     }
+
+    val timer = object: CountDownTimer(3000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {}
+
+        override fun onFinish() {
+            val intent = Intent("android.intent.action.main_act")
+            startActivity(intent)
+        }
+    }
+
 }
